40adb264539b53e9fcbeb66aa5007857ddeea3a1
yota morimoto <yotamorimoto@gmail.com>
2020-12-26 14:33:56 +0900

    Delete test_brown.js

--- test_brown.js
+++
@@ -1,9 +0,0 @@
-import { Brown } from './brown.js'
-import { seed } from './random.js'
-
-// seed(0)
-let b = new Brown(3)
-
-console.log(b.next(1,0.1))
-console.log(b.next(1,0.1))
-console.log(b.next(1,0.1))
